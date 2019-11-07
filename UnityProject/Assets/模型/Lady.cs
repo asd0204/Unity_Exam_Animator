e5f5272071dc48496ad2fa779aefbdc1cfce28d6
asd0204 <55218578+asd0204@users.noreply.github.com>
2019-11-07 09:56:39 +0800

    動態捕捉_B1040462_邱歆涵

---
+++ UnityProject/Assets/模型/Lady.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Lady : MonoBehaviour
+{
+
+    // 修飾詞 傳回類型 方法名稱 (參數) { 敘述 }
+    // void 為無傳回類型
+
+
+    /// <summary>
+    /// 跑步
+    /// </summary>
+    private void Wark()
+    {
+
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    private void Hurt()
+    {
+
+    }
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+}
+
---
+++ UnityProject/Assets/模型/SsdeCS.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SsdeCS : MonoBehaviour
+{
+    private Animator m_Animator;
+
+
+
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            m_Animator.SetTrigger("跳舞觸發");
+        m_Animator.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+
+    }
+}
+
